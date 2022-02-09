b9c55cc9589d7fa63944100d01d6c3f603b232af
Travis Helms <smasher.coder@gmail.com>
2022-02-08 23:34:41 -0600

    end of 21.5

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Auth from '../../utils/auth';
 
 const Header = () => {
   return (
@@ -9,13 +10,29 @@ const Header = () => {
           <h1>Deep Thoughts</h1>
         </Link>
 
-        <nav className="text-center">
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Signup</Link>
-        </nav>
+    <nav className="text-center">
+      {Auth.loggedIn() ? (
+      <>
+      <Link to="/profile">Me</Link>
+      <a href="/" onClick={logout}>
+        Logout
+      </a>
+      </>
+      ) : (
+      <>
+      <Link to="/login">Login</Link>
+      <Link to="/signup">Signup</Link>
+      </>
+      )}
+    </nav>
       </div>
     </header>
   );
+};
+
+const logout = event => {
+  event.preventDefault();
+  Auth.logout();
 };
 
 export default Header;
